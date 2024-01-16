78c3259e3b62e1de67d0175c57fdf545f314951b
JSpiner <jspiner@naver.com>
2024-01-16 20:44:55 +0900

    구글폼 링크 변경

--- src/Const.tsx
+++ src/Const.tsx
@@ -1,4 +1,4 @@
-export const APPLY_LINK = "https://forms.gle/uCiShw8MLKmBtfu16";
+export const APPLY_LINK = "https://forms.gle/fUwUkf6cmVSh9amd8";
 export const APPLY_GUIDE_LINK =
   "https://jspiner.notion.site/b00b3e7a944b4d088409f3be9d616649";
 export const INTERVIEWER_GUIDE_LINK =
