1f4fbba13f7cae40faba785f39403da57c50da37
smith.jspiner <smith.jspiner@kakaomobility.com>
2024-01-16 20:21:07 +0900

    firebase 로깅 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics, logEvent } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+}
+console.log(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const queryParameters = new URLSearchParams(window.location.search)
+const utmSource = queryParameters.get("utm_source")
+logEvent(analytics, 'ref_utm_source', {
+  content_id: utmSource
+});
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
